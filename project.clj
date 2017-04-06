4e000f5eb2a02356037b41f5ee940a3b94211ce3
Brydon Cheyney <brydon@remission.org.uk>
2017-04-06 23:36:44 +0100

    Brydon - undo upgrade to project's clojure version dependency to avoid placing burden of support on consumer

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Less tolerant Clojure."
   :url "https://github.com/ctford/strictly"
   :license {:name "MIT License"}
-  :dependencies	[[org.clojure/clojure "1.8.0"]]
+  :dependencies	[[org.clojure/clojure "1.5.1"]]
   :profiles {:dev
              {:plugins [[lein-midje "3.2.1"] ]
               :dependencies [[midje "1.8.3"]]}})
